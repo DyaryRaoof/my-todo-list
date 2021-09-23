a7ef28f42568cd51b7fa10191735b7cacab38389
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-09-24 00:04:18 +0300

    Added functionality to edit , delete, complete tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import update from './update.js';
+import { addTodo, deleteTodo, updateTodo } from './crud';
 
 const button = document.querySelector('button');
 class Todo {
@@ -10,32 +11,50 @@ class Todo {
   }
 }
 
-let todos = [
-  new Todo('Hello world 1', false, 0),
-  new Todo('Hello world 2', false, 1),
-  new Todo('Hello world 5', false, 4),
-  new Todo('Hello world 3', false, 2),
-  new Todo('Hello world 4', false, 3),
-];
+let todos = [];
 
-function populate() {
-  todos.sort((a, b) => (a.index > b.index ? 1 : -1));
-  todos.forEach((todo) => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <div class="flex">
+function createTodoElement(todo) {
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <div class="flex todo-element">
       <div>
           <input type="checkbox" class="checkbox" 
           ${todo.completed ? 'checked' : ''}>
           <span>${todo.description}</span>
       </div>
-      <span class="material-icons">
+      <span class="material-icons edit-icon" style="cursor: pointer">
           more_vert
       </span>
     </div>
     <hr>`;
 
-    button.parentElement.insertBefore(li, button);
+  return li;
+}
+
+function createReplaceTodoElement(todo) {
+  const html = `
+  <div>
+    <input type="checkbox" class="checkbox" 
+    ${todo.completed ? 'checked' : ''}>
+    <span>${todo.description}</span>
+  </div>
+  <span class="material-icons edit-icon" style=" cursor: pointer">
+      more_vert
+  </span>
+    `;
+
+  return html;
+}
+
+function addTodoElement(todo) {
+  const li = createTodoElement(todo);
+  button.parentElement.insertBefore(li, button);
+}
+
+function populate() {
+  todos.sort((a, b) => (a.index > b.index ? 1 : -1));
+  todos.forEach((todo) => {
+    addTodoElement(todo);
   });
 }
 
@@ -43,12 +62,103 @@ function saveTodosLocally() {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-function addEventsToCheckboxes() {
+function createReplaceTodoElementForCompletedTask(todo) {
+  const html = `
+  
+  <div>
+  <span class="material-icons edit-icon" style=" cursor: pointer; color: green">
+      done
+  </span>
+    <strike><span>${todo.description}</span></strike>
+  </div>
+  <span class="material-icons edit-icon" style=" cursor: pointer">
+      more_vert
+  </span>
+    `;
+
+  return html;
+}
+
+function changeElementToCompleted(index) {
+  update(todos[index]);
+  saveTodosLocally();
+  if (todos[index].completed) {
+    const completedElement = createReplaceTodoElementForCompletedTask(todos[index]);
+    const todoElements = document.querySelectorAll('.todo-element');
+    todoElements[index].innerHTML = completedElement;
+  }
+}
+
+function addEventsToCheckboxes(recievedIndex) {
   const checkboxes = document.querySelectorAll('.checkbox');
+
   checkboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener('change', () => {
-      update(todos[index]);
-      saveTodosLocally();
+    if (recievedIndex) {
+      if (recievedIndex === index) {
+        checkbox.addEventListener('change', () => {
+          changeElementToCompleted(index);
+        });
+      }
+    } else {
+      checkbox.addEventListener('change', () => {
+        changeElementToCompleted(index);
+      });
+    }
+  });
+}
+
+function addEventsToEditIcons() {
+  const editIcons = document.querySelectorAll('.edit-icon');
+  const todoElements = document.querySelectorAll('.todo-element');
+
+  todos.forEach((todo, index) => {
+    editIcons[index].addEventListener('click', () => {
+      const div = document.createElement('div');
+      div.classList.add('flex', 'todo-element');
+      div.style.backgroundColor = '#FFFBAE';
+
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.classList.add('checkbox');
+      checkbox.checked = todo.completed;
+
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.classList.add('edit-input');
+      input.value = todo.description;
+      input.style.backgroundColor = 'transparent';
+
+      const span = document.createElement('span');
+      span.classList.add('material-icons', 'edit-icon');
+      span.style.marginLeft = 'auto';
+      span.style.cursor = 'pointer';
+      span.innerHTML = 'delete';
+
+      div.appendChild(checkbox);
+      div.appendChild(input);
+      div.appendChild(span);
+
+      todoElements[index].replaceWith(div);
+
+      input.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter') {
+          const todo = todos[index];
+          todo.description = input.value;
+          updateTodo(todo, todos[index]);
+          const html = createReplaceTodoElement(todo);
+          div.innerHTML = html;
+          addEventsToEditIcons();
+          saveTodosLocally();
+          div.style.backgroundColor = 'white';
+        }
+      });
+
+      span.addEventListener('click', () => {
+        saveTodosLocally();
+        deleteTodo(todo, todos);
+        div.parentElement.remove();
+        saveTodosLocally();
+      });
     });
   });
 }
@@ -60,4 +170,39 @@ window.addEventListener('load', () => {
   }
   populate();
   addEventsToCheckboxes();
+  addEventsToEditIcons();
+});
+
+function addEventListenerToInput() {
+  const input = document.querySelector('#input');
+  input.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+      const todo = new Todo(input.value, false, todos.length);
+      addTodo(todo, todos);
+      addTodoElement(todo);
+      saveTodosLocally();
+      input.value = '';
+      addEventsToEditIcons(todos.length);
+      addEventsToCheckboxes(todos.length - 1);
+    }
+  });
+}
+
+addEventListenerToInput();
+
+button.addEventListener('click', () => {
+  const todoElements = document.querySelectorAll('.todo-element');
+  const removedTodos = [];
+  for (let i = 0; i < todos.length; i += 1) {
+    if (todos[i].completed === true) {
+      removedTodos.push(todos[i]);
+      todoElements[i].parentNode.remove();
+    }
+  }
+
+  removedTodos.forEach((todo) => {
+    deleteTodo(todo, todos);
+  });
+
+  saveTodosLocally();
 });
